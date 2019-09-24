625d5a0e2613f7f51737f495dfa4e8735181d568
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 18:38:51 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan10logika/PBOIF210118058Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan10logika;
+
+/**
+ *
+ * @author user
+ *  * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa=(umur>=17);
+        System.out.println("Status dewasa = " + dewasa);
+    }
+    
+}
